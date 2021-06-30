8074dab2f4780fb31398f7b973b7cb0929e08c98
wolfryan97 <wolf.ryan97@gmail.com>
2021-06-30 17:09:15 -0400

    View Routine added
    added css, js, html for view routine framework

---
+++ scripts/view_routine.js
@@ -0,0 +1,13 @@
+const routine = {
+    routineId : 0,
+    username : "",
+    routineName : "",
+    dateScheduled : 0,
+    dateCompleted : 0
+}
+
+
+function routineName(){
+    document.getElementById("routinename").innerHTML = "Test Routine"
+}
+routineName()
