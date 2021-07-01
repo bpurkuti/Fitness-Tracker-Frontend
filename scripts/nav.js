3aa07f38cfdc94a60e81e2d660aac31cc24639ce
bpurkuti <bishwopurkutihhs@gmail.com>
2021-07-01 14:07:45 -0700

    Fixed home and login buttons on navbar/Added different functions for some pages

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,22 +1,40 @@
 const nav = document.getElementById("navbar");
+const adminnav = document.getElementById("adminnavbar");
+const signupnav = document.getElementById("signupnavbar");
 
 function loadNavBar() {
-	nav.innerHTML = `
-    <div> 
-        <a href = "" id = "navhome" class="here" onclick= "home()">Home</a>  
-        <a href = "../login.html" id = "navlogout" onclick= "logout()"  >Logout</a>
-    </div>
-    `;
+	if (nav) {
+		nav.innerHTML = `
+        <div> 
+            <a href = "../dashboard.html" id = "navhome" class="here">Home</a>  
+            <a href = "../login.html" id = "navlogout" onclick= "logout()"  >Logout</a>
+        </div>
+        `;
+	}
+
+	if (adminnav) {
+		adminnav.innerHTML = `
+        <div> 
+            <a href = "../login.html" id = "navlogout" onclick= "logout()"  >Logout</a>
+        </div>
+        `;
+	}
+
+	if (signupnav) {
+		signupnav.innerHTML = `
+        <div> 
+            <a href = "../login.html" id = "navhome" class="here">Home</a>  
+        </div>
+        `;
+	}
 }
 
 function logout() {
 	//Write code to handle logout here
-	window.location.assign("login.html");
-}
+	//Clear credentials and invalidate session
 
-function home() {
-	//Write code to handle logout here
-	window.location.assign("dashboard.html");
+	//Remove this when implementing
+	window.location.assign("login.html");
 }
 
 loadNavBar();
