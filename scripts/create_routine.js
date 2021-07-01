125d0b247d6296f47d5352629916d635a21667b8
bpurkuti <bishwopurkutihhs@gmail.com>
2021-07-01 13:46:52 -0700

    Added filter options when creating routine

--- scripts/create_routine.js
+++ scripts/create_routine.js
@@ -3,27 +3,50 @@ const inputexcercise = document.getElementById("inputexcercise");
 const searchexcercise = document.getElementById("searchexcercise");
 const addexcercisebtn = document.getElementById("addexcercisebtn");
 const addedexcerciselist = document.getElementById("addedexcerciselist");
+const filterbytype = document.getElementById("filterbytype");
 const description = document.getElementById("description");
 const submitbtn = document.getElementById("submitbtn");
 const submitmsg = document.getElementById("submitmsg");
 
-let excerciseoptions = "";
+let excerciseoptions;
 const excerciselist = [
-	"Squats",
-	"Preacher Curls",
-	"Treadmill",
-	"Pullups",
-	"Benchpress",
-	"Pushups",
-	"Ladder",
-	"Bike",
-	"Leg curls",
-	"Leg press",
+	{ name: "Preacher Curls", type: "strength" },
+	{ name: "Treadmill", type: "cardio" },
+	{ name: "Pullups", type: "strength" },
+	{ name: "calf", type: "stretch" },
+	{ name: "Benchpress", type: "strength" },
+	{ name: "Pushups", type: "strength" },
+	{ name: "Ladder", type: "cardio" },
+	{ name: "Bike", type: "cardio" },
+	{ name: "Leg curls", type: "strength" },
+	{ name: "Leg press", type: "strength" },
+	{ name: "hamstring", type: "stretch" },
 ];
+let filteredexcerciselist = [];
 let addedexcercises = [];
-function onLoad() {
-	for (let i = 0; i < excerciselist.length; i++) {
-		excerciseoptions += `<option value="${excerciselist[i]}">`;
+
+//Theres some filterning going on here atm. Need to change it up depending on how we store excercises
+function setExcerciseList(filtertype) {
+	excerciseoptions = "";
+
+	if (filtertype === "strength") {
+		filteredexcerciselist = excerciselist.filter(
+			(excercise) => excercise["type"] === "strength"
+		);
+	} else if (filtertype === "cardio") {
+		filteredexcerciselist = excerciselist.filter(
+			(excercise) => excercise["type"] === "cardio"
+		);
+	} else if (filtertype === "stretch") {
+		filteredexcerciselist = excerciselist.filter(
+			(excercise) => excercise["type"] === "stretch"
+		);
+	} else {
+		filteredexcerciselist = excerciselist;
+	}
+
+	for (let i = 0; i < filteredexcerciselist.length; i++) {
+		excerciseoptions += `<option value="${filteredexcerciselist[i]["name"]}">`;
 	}
 
 	searchexcercise.innerHTML = excerciseoptions;
@@ -51,4 +74,8 @@ function addExcerciseToList() {
 	addedexcerciselist.innerHTML = excercises;
 	console.log(addedexcercises);
 }
-onLoad();
+
+filterbytype.onchange = () => {
+	setExcerciseList(filterbytype.value);
+};
+setExcerciseList(filterbytype.value);
