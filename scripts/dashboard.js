d3a6d7bf212b272b0f52c55db202affc3f4d64e6
bpurkuti <bishwopurkutihhs@gmail.com>
2021-07-02 14:53:33 -0700

    Implemented dashboard with endpoint

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -2,25 +2,51 @@ const submitMsg = document.getElementById("submitmsg");
 const pageTitle = document.getElementById("pagetitle");
 const routineList = document.getElementById("routinelist");
 let routines = "";
-function listRoutines() {
-	//Load All Routines
-	//run this function on every page reload
-	for (let i = 1; i <= 5; i++) {
-		routines += `<button class = "rbtn" id = "routine${i}" value = ${i} onclick = "viewRoutine(this.value)">Routine${i}</button>`;
+let routinesList = [];
+let serverUrl = "http://localhost:7000/";
+
+async function listRoutines() {
+	const config = {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ session: sessionStorage.getItem("session") }),
+	};
+	const response = await fetch(`${serverUrl}getRoutinesForUser`, config);
+	const routines = await response.json();
+
+	//I am putting every data in list in case we want to present them with more detail in future
+	for (let routine of routines) {
+		let r = {
+			routineId: routine["routineId"],
+			username: routine["username"],
+			routineName: routine["routineName"],
+			dateScheduled: routine["dateScheduled"],
+		};
+		routinesList.push(r);
 	}
-	routineList.innerHTML = routines;
+
+	console.log(routinesList);
+	let routineBtns = "";
+	for (let i = 0; i < routinesList.length; i++) {
+		routineBtns += `<button class = "rbtn" id ="routine${routinesList[i]["routineId"]}"  value = "${routinesList[i]["routineId"]}" onclick = "view_routine(this.value)">${routinesList[i]["routineName"]}</button>`;
+	}
+	routineList.innerHTML = routineBtns;
 }
 
-//Update current routineId and view_routine page
-function viewRoutine(rId) {
-	sessionStorage.setItem("currRId", rId);
+async function welcomeTitle() {
+	const config = {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ session: sessionStorage.getItem("session") }),
+	};
+	const response = await fetch(`${serverUrl}getAccount`, config);
+	const body = await response.json();
+	pageTitle.innerHTML = `Welcome ${body["firstName"]} ${body["lastName"]}`;
+}
+
+function view_routine(val) {
+	sessionStorage.setItem("currRoutineId", val);
 	window.location.assign("view_routine.html");
-}
-
-function welcomeTitle() {
-	const fName = "Wolf";
-	const lName = "Ryan";
-	pageTitle.innerHTML = `Welcome ${fName} ${lName}`;
 }
 
 function addRoutine() {
