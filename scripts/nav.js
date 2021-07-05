3163834b3e07388a2d9d7b8c21e2fd1b94695840
JonathanLemarroy <jonathanlemarroy4399@gmail.com>
2021-07-05 15:40:24 -0500

    Added a working view routine page

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,4 +1,14 @@
 async function logOut() {
-	window.location.assign("login.html");
+	const config = {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ session: sessionStorage.getItem("session") }),
+	};
+	const response = await fetch(`${serverUrl}logoutAccount`, config);
 	sessionStorage.clear();
+	window.location.assign("login.html");
+}
+
+async function gotoHome() {
+	window.location.assign("dashboard.html");
 }
